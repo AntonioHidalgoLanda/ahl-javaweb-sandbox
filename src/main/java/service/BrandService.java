a1ed7b0c2ba262be206892b4a3052d23af93de75
AntonioHL <hidalgo.landa@gmail.com>
2017-07-29 14:31:27 +0100

    modularizing services: brands - minor amends

--- src/main/java/service/BrandService.java
+++ src/main/java/service/BrandService.java
@@ -19,11 +19,13 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.ResponseBody;
+import org.springframework.web.bind.annotation.RestController;
 
 /**
  *
  * @author antonio
  */
+@RestController
 public class BrandService {
     private BasicDataSource connectionPool;
     
