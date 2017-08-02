535ab11606e6629b17d0135722827f2fe2cd3015
AntonioHL <hidalgo.landa@gmail.com>
2017-08-02 18:40:16 +0100

    Debuging modular services

--- src/main/java/product/Application.java
+++ src/main/java/product/Application.java
@@ -17,7 +17,7 @@ import org.springframework.context.annotation.Bean;
  *
  * @author antonio
  */
-@SpringBootApplication
+@SpringBootApplication(scanBasePackages = {"service"})
 public class Application {
 
     public static void main(String[] args) {
